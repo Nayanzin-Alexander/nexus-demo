4f36e65983e214808ae0632729f932dd7a719d8d
oleksandr-nayanzin <oleksandr.nayanzin@thomsonreuters.com>
2020-05-11 13:31:33 +0300

    + added separate project + added separate project to the main as a maven dependency through nexus repository

---
+++ common-lib/src/main/java/com/nayanzin/commonlib/CommonLibDto.java
@@ -0,0 +1,9 @@
+package com.nayanzin.commonlib;
+
+import lombok.Data;
+
+@Data
+public class CommonLibDto {
+    private Long id;
+    private String name;
+}
--- sprin-boot-example/src/test/java/com/nayanzin/sprinbootexample/SprinBootExampleApplicationTests.java
+++ sprin-boot-example/src/test/java/com/nayanzin/sprinbootexample/SprinBootExampleApplicationTests.java
@@ -1,5 +1,8 @@
 package com.nayanzin.sprinbootexample;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.nayanzin.commonlib.CommonLibDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,15 @@ class SprinBootExampleApplicationTests {
 
     @Test
     void contextLoads() {
+        CommonLibDto expectedDto = new CommonLibDto();
+        expectedDto.setId(1L);
+        expectedDto.setName("Name");
+
+        CommonLibDto actualDto = new CommonLibDto();
+        actualDto.setId(1L);
+        actualDto.setName("Name");
+
+        assertThat(actualDto).isEqualTo(expectedDto);
     }
 
 }
