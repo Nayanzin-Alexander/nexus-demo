e61effb212d908170dfcfb317e47529fb028fc3f
oleksandr-nayanzin <oleksandr.nayanzin@thomsonreuters.com>
2020-05-11 12:34:36 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+Using Nexus 3 as Your Repository
+https://blog.sonatype.com/using-nexus-3-as-your-repository-part-1-maven-artifacts
---
+++ sprin-boot-example/src/test/java/com/nayanzin/sprinbootexample/SprinBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayanzin.sprinbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
